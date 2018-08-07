0d38682969cd1bdbedf00ba02afe6acb7eb311e8
Caitlin Kuiper <caitlinmkuiper@gmail.com>
2018-08-08 01:59:39 +0200

    added form buttons

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@
 $(document).ready(function() {
   $('.drawer').drawer();
 });
+
+//Disabled Button
+$(function(){
+  $('.input').keyup(function() {
+
+    var empty = false;
+    $('.input').each(function(){
+      if ($(this).val() == '') {
+        empty = true;
+      }
+    });
+
+    if (empty) {
+      $('#submit').attr('disabled', 'disabled').hasClass('.button--disabled').removeClass('button--primary');
+    } else {
+      $('#submit').removeAttr('disabled').addClass('button--primary').removeClass('button--disabled');
+    }
+  });
+})()
